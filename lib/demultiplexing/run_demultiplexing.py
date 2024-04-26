9794134d344bac515bdaa9dd4744ef791c64d8aa
viols-code <viola.renne@gmail.com>
2024-04-26 20:37:45 +0200

    Demultiplexing

--- lib/demultiplexing/run_demultiplexing.py
+++ lib/demultiplexing/run_demultiplexing.py
@@ -3,53 +3,86 @@ from Bio import SeqIO
 from Bio.Align import sam
 from lib.general_helpers.run_command import run_command
 import pandas as pd
-
-# matK = SeqIO.read("Sequences/matK.fasta", "fasta").seq
-# rbcL = SeqIO.read("Sequences/rbcL.fasta", "fasta").seq
-# psbA_trnH = SeqIO.read("Sequences/psbA-trnH.fasta", "fasta").seq
-# ITS = SeqIO.read("Sequences/ITS.fasta", "fasta").seq
-
-count = 0
-for record in SeqIO.parse("../../data/fake_multiplexed/Ficus_religiosa_mutiplexed.fastq", "fastq"):
-    count += 1
-print(count)
-
-logger = getLogger('ciao')
-
-genes = ['matK', 'rbcL', 'psbA-trnH', 'ITS']
-
-alignment_paths = []
-
-for gene in genes:
-    reference_path = f"Sequences/Ficus_religiosa/{gene}.fasta"
-    read_path = "../../data/fake_multiplexed/Ficus_religiosa_mutiplexed.fastq"
-    alignment_path = f"Alignment/Ficus_religiosa/{gene}.sam"
-    minimap2_command = f"minimap2 -ax map-ont {reference_path} {read_path} > {alignment_path}"
-    _, minimap2_time = run_command(minimap2_command, logger, windows=False)
-    alignment_paths.append(alignment_path)
+import shutil
 
 
-data = {'matK_seq_id': [], 'matK_flag': [], 'matK_mapq': [], 'rbcL_seq_id': [], 'rbcL_flag': [], 'rbcL_mapq': [],
-        'psbA-trnH_seq_id': [], 'psbA-trnH_flag': [], 'psbA-trnH_mapq': [],
-        'ITS_seq_id': [], 'ITS_flag': [], 'ITS_mapq': []}
+def demultiplexing(input_fastq, reference_paths, output_folder):
+    """
+    Demultiplexing the fastq file containing the reads.
+    It saves one fastq file for each gene, demultiplexing the reads.
 
-c = 0
-for alignment_path in alignment_paths:
-    print(alignment_path)
-    alignment_iterator = sam.AlignmentIterator(alignment_path)
-    l = 0
-    for alignment in alignment_iterator:
-        if alignment.sequences[1].id in data[f'{genes[c]}_seq_id']:
-            if bin(alignment.flag)[2:].zfill(12)[0] == 1 and bin(alignment.flag)[2:].zfill(12)[3] == 1:
-                pass
+    Args:
+        input_fastq: input fastq file containing the multiplexed reads
+        reference_paths: paths with reference sequences for each gene
+        output_folder: output folder path for saving each final fastq file
+    """
+    logger = getLogger('demultiplexing')
+
+    # Get genes' names from the reference paths
+    genes = []
+    for path in reference_paths:
+        genes.append(path.split('.')[0])
+
+    # Deliting output directiory
+    shutil.rmtree("Alignment", ignore_errors=False)
+
+    # Create SAM files for each gene
+    alignment_paths = []
+    for gene, reference_path in zip(genes, reference_paths):
+        alignment_path = f"Alignment/{gene}.sam"
+        minimap2_command = f"minimap2 -ax map-ont {reference_path} {input_fastq} > {alignment_path}"
+        _, minimap2_time = run_command(minimap2_command, logger, windows=False)
+        alignment_paths.append(alignment_path)
+
+    # Create empy dictionary to store information from SAM files
+    data = {}
+    for gene in genes:
+        data[f'{gene}_seq_id'] = []
+        data[f'{gene}_flag'] = []
+        data[f'{gene}_mapq'] = []
+
+    # Get the sequence id, flag and map quality from SAM files
+    for gene, alignment_path in zip(genes, alignment_paths):
+        print(alignment_path)
+        # Get the iterator on the alignments
+        alignment_iterator = sam.AlignmentIterator(alignment_path)
+        for alignment in alignment_iterator:
+            if alignment.sequences[1].id in data[f'{gene}_seq_id']:  # Check it is not duplicated
+                if bin(alignment.flag)[2:].zfill(12)[0] == 1 and bin(alignment.flag)[2:].zfill(12)[3] == 1:
+                    pass
+            else:
+                data[f'{gene}_seq_id'].append(alignment.sequences[1].id)
+                data[f'{gene}_flag'].append(alignment.flag)
+                data[f'{gene}_mapq'].append(alignment.mapq)
+
+    # Create the dataframe from the dictionary
+    sam_out = pd.DataFrame.from_dict(data)
+    # Get the gene with the best map quality score
+    sam_out['mapq_max'] = sam_out.iloc[:, 2::3].idxmax(axis=1)
+    sam_out['unmapped'] = sam_out.iloc[:, 2::3].max(axis=1)
+
+    # Opening output files for each gene
+    output_files = []
+    for gene in genes:
+        output_files.append(open(f'{output_folder}/{gene}', 'w'))
+
+    # Saving each read in the correct fastq file
+    index = 0
+    for read in SeqIO.parse(input_fastq, "fastq"):
+        if sam_out.iloc[index]['unmapped'] == 0:  # Flag = 0 -> unmapped read, so we discard it
+            pass
         else:
-            data[f'{genes[c]}_seq_id'].append(alignment.sequences[1].id)
-            data[f'{genes[c]}_flag'].append(alignment.flag)
-            data[f'{genes[c]}_mapq'].append(alignment.mapq)
-    print(l + count)
-    c += 1
+            gene_name = sam_out.iloc[index]['mapq_max'][0:-5]
+            position = genes.index(gene_name)
+            SeqIO.write(read, output_files[position], 'fastq')
+        index += 1
 
-ciao = pd.DataFrame.from_dict(data)
-print(ciao.head())
+    # Closing the output files
+    for output_file in output_files:
+        output_file.close()
+
+    # Deliting output directiory
+    shutil.rmtree("Alignment", ignore_errors=False)
+
 
 
